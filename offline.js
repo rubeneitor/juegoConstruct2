422f7852dbfbd48d85b4631a60f6e052c0169c44
Ruben Viosquez <rvb1492@hotmail.com>
2019-10-15 16:53:49 +0200

    Juego construct

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1571151114,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/backscreen-sheet0.png",
+		"images/edge-sheet0.png",
+		"images/player-sheet0.png",
+		"images/ball1-sheet0.png",
+		"images/brick1-sheet0.png",
+		"images/brick2-sheet0.png",
+		"images/brick3-sheet0.png",
+		"images/life1-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
